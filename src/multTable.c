d37e45ae0b477fa11c9dd483008a22ffb4adec44
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-16 16:22:13 +0500

    i change it but program can't show output

--- src/multTable.c
+++ src/multTable.c
@@ -7,21 +7,46 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include<math.h>
 
 int main(int argc, char **argv) {
 
-  if(argc != 2) {
-    printf("Error: provide an integer\n");
-    exit(1);
-  }
+  // if(argc != 2) {
+  //   printf("Error: provide an integer\n");
+  //   exit(1);
+  // }
 
-  int n = atoi(argv[1]);
-  if(n <= 0) {
-    printf("empty table!\n");
-    exit(0);
-  }
+  // int n = atoi(argv[1]);
+  // if(n <= 0) {
+  //   printf("empty table!\n");
+  //   exit(0);
+  // }
 
   //TODO: place your code here
+  //write my self code
 
+  int number,i,j;
+
+  printf("Please enter Number");
+  scanf("%d", &number);
+
+  for( i=1; i<=number; i++){
+    printf("%4d",i);
+
+  }
+  printf("\n");
+
+  //display the table
+
+  for( i=1; i<=number; i++){
+    printf("%2d |", i);
+
+    for(j=1; j<=number; j++){
+      printf("%4d ",i*j);
+    }
+    printf("\n");
+  }
+  
+ 
   return 0;
 }
