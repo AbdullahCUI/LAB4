ca3f90f44caf3c79130b0d12279ac165870af11b
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-18 00:50:21 +0500

    condition and expression in for loop

--- src/exercises.c
+++ src/exercises.c
@@ -166,7 +166,7 @@ for(int i=1; i<=n; i++){
 
 //using for loop
 //declared variable
-  for(int i=1;)
+  for(int i=1; i<=n; i++){}
 
   //6. Compute the summation of squares of numbers 1 thru n
   //   and print the result; example: 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 55
