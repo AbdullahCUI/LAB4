46bafb7ab9391d39b1c6b1378381739be85a9cce
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-18 01:16:25 +0500

    use printf function

--- src/exercises.c
+++ src/exercises.c
@@ -215,6 +215,10 @@ for(int i=1; i<=n; i++){
 
   int sum=0;
 
+  //use printf function
+
+  printf("Square of the number:")
+
   //7. Compute the summation of even numbers 1 thru n and print
   //   the result; example (n=5): 2 + 4 = 6
 
