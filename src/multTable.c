f4a3a1e4309cd37b626f6f062d13cb1466de40f5
Abdullah-0110 <abdullahdev88@gmail.com>
2023-10-18 02:18:47 +0500

    declared variable

--- src/multTable.c
+++ src/multTable.c
@@ -1,6 +1,6 @@
 /**
  * Author: Abdullah
- * Date: 16\10\2023
+ * Date: 17\10\2023
  *
  * This program generates a multiplication table
  * of the size given as a command line argument.
@@ -16,26 +16,6 @@ int main(int argc, char **argv) {
   //write my self code
 
   int number,i,j;
-
-  printf("Please enter Number");
-  scanf("%d", &number);
-
-  for( i=1; i<=number; i++){
-    printf("%4d",i);
-
-  }
-  printf("\n");
-
-  //display the table
-
-  for( i=1; i<=number; i++){
-    printf("%2d |", i);
-
-    for(j=1; j<=number; j++){
-      printf("%4d ",i*j);
-    }
-    printf("\n");
-  }
   
  
   return 0;
